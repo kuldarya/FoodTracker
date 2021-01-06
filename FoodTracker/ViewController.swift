8784121f39c5f9d0f54efb632ac3f69d903b5d11
Darya Kuliashova <kuldiand@gmail.com>
2021-01-06 15:12:10 -0800

    Create remote repo

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Darya Kuliashova on 9.12.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
