0659a5eae2a06d7d7748324093c2c61a4db44e5f
Darya Kuliashova <kuldiand@gmail.com>
2021-01-11 17:11:05 -0800

    UI added

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,12 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet private weak var mealTextField: UITextField!
+    @IBOutlet private weak var mealName: UILabel!
+    @IBOutlet weak var setDefaultTextButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mealName.text = "Meal Name"
+        setDefaultTextButton.setTitle("Set Default Text for Label", for: .normal)
+        
+        setupMealTextField()
     }
-
-
+    
+    //MARK: - Setup
+    private func setupMealTextField() {
+        mealTextField.delegate = self
+        mealTextField.returnKeyType = .done
+        mealTextField.enablesReturnKeyAutomatically = true
+    }
+    
+    @IBAction func SetDefaultTextButtonTapped(_ sender: Any) {
+    }
 }
 
