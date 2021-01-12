24e94adddcff26eeb95d1a166ef2fedd5b7c4663
Darya Kuliashova <kuldiand@gmail.com>
2021-01-11 17:57:31 -0800

    UITextFieldDelegate extension added

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -7,28 +7,44 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate {
+class ViewController: UIViewController {
     @IBOutlet private weak var mealTextField: UITextField!
-    @IBOutlet private weak var mealName: UILabel!
-    @IBOutlet weak var setDefaultTextButton: UIButton!
+    @IBOutlet private weak var mealNameLabel: UILabel!
+    @IBOutlet private weak var setDefaultLabelTextButton: UIButton!
     
+    //MARK: -
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        mealName.text = "Meal Name"
-        setDefaultTextButton.setTitle("Set Default Text for Label", for: .normal)
-        
-        setupMealTextField()
+        mealTextField.delegate = self
+        setupUI()
     }
     
     //MARK: - Setup
-    private func setupMealTextField() {
-        mealTextField.delegate = self
+    private func setupUI() {
         mealTextField.returnKeyType = .done
         mealTextField.enablesReturnKeyAutomatically = true
+        mealTextField.autocapitalizationType = .sentences
+        mealNameLabel.text = "Meal Name"
+        setDefaultLabelTextButton.setTitle("Set Default Text for Label", for: .normal)
     }
     
-    @IBAction func SetDefaultTextButtonTapped(_ sender: Any) {
+    //MARK: - Actions
+    @IBAction private func setDefaultLabelText(_ sender: UIButton) {
+        mealNameLabel.text = "Default text"
+        mealTextField.text = ""
+    }
+}
+
+//MARK: - Extensions
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
     }
 }
 
